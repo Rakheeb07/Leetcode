25bbef129b73ea6b30e532a428ae1f7131418fe6
ABDUL RAKHEEB <23a91a0569@aec.edu.in>
2026-08-08 14:40:16 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (50.87%) - LeetSync

--- 4177-reverse-string-prefix/reverse-string-prefix.py
+++ 4177-reverse-string-prefix/reverse-string-prefix.py
@@ -1,12 +1,10 @@
 class Solution:
     def reversePrefix(self, s: str, k: int) -> str:
-        arr = list(s)
-        l = 0
-        r = k - 1
-
-        while l < r:
-            arr[l], arr[r] = arr[r], arr[l]
-            l += 1
-            r -= 1
-
-        return "".join(arr)
+        a=list(s)
+        l=0
+        r=k-1
+        while l<r:
+           a[l],a[r]=a[r],a[l]
+           l+=1
+           r-=1
+        return "".join(a)
